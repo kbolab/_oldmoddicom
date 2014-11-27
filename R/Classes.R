a1efb2f07421333bfb4593dd8458dcf3dbe360f7
Nicola <nicola.dinapoli.74@gmail.com>
2014-11-27 19:12:16 +0100

    Add modeling files

---
+++ R/Classes.R
@@ -0,0 +1,8 @@
+setClass("dvhmatrix", 
+         representation(
+           dvh="matrix",                # matrix of DVHs, 1st column is dose, other columns are the volume
+           dvh.type="character",        # cumulative or differential DVH
+           vol.distr="character",       # absolute or relative distribution of the volume
+           volume="numeric"             # vector of absolute volumes of DVH
+           )
+         )
